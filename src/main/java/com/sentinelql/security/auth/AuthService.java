26751487c77fcb638ffb651e12d104a61b407fe8
Rocard Fonji <fonkengrocard@gmail.com>
2024-01-15 22:26:41 +0100

    (implement-auth-controller): implement register method

--- src/main/java/com/sentinelql/security/auth/AuthService.java
+++ src/main/java/com/sentinelql/security/auth/AuthService.java
@@ -1,13 +1,38 @@
 package com.sentinelql.security.auth;
 
+import com.sentinelql.security.config.JwtService;
+import com.sentinelql.security.user.Role;
+import com.sentinelql.security.user.User;
+import com.sentinelql.security.user.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import static com.sentinelql.security.user.Role.USER;
 
 @Service
 @RequiredArgsConstructor
 public class AuthService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
     public AuthenticationResponse register(RegisterRequest request) {
-        return null;
+        var user = User.builder()
+                .firstname(request.getFirstname())
+                .lastname(request.getLastname())
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .role(USER)
+                .build();
+
+        userRepository.save(user);
+
+        var jwtToken = jwtService.generateToken(user);
+
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
     }
 
     public AuthenticationResponse authenticate(AuthenticationRequest request) {
