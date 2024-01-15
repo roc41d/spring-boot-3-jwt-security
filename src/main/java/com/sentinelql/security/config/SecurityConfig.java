02f05e8689da51dd28697a93b34e2706287544ca
Rocard Fonji <fonkengrocard@gmail.com>
2024-01-15 22:04:17 +0100

    (implement-auth-controller): create auth contoller, authe response, register request and auth request objects

--- src/main/java/com/sentinelql/security/config/SecurityConfig.java
+++ src/main/java/com/sentinelql/security/config/SecurityConfig.java
@@ -1,12 +1,12 @@
 package com.sentinelql.security.config;
 
+import jakarta.servlet.Filter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -18,7 +18,7 @@ import static org.springframework.security.config.http.SessionCreationPolicy.STA
 public class SecurityConfig {
 
     private static final String[] WHITELIST_URL = {};
-    private final JwtAuthenticationFilter jwtAuthFilter;
+    private final Filter jwtAuthFilter;
     private final AuthenticationProvider authenticationProvider;
 
     @Bean
