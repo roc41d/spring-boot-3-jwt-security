27be28f54df0df9d80755b6fc40abe198e1a6fcc
Rocard Fonji <fonkengrocard@gmail.com>
2024-01-15 22:33:07 +0100

    (implement-auth-controller): implement authentication method

--- src/main/java/com/sentinelql/security/auth/AuthService.java
+++ src/main/java/com/sentinelql/security/auth/AuthService.java
@@ -5,6 +5,8 @@ import com.sentinelql.security.user.Role;
 import com.sentinelql.security.user.User;
 import com.sentinelql.security.user.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -17,6 +19,7 @@ public class AuthService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
     private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
     public AuthenticationResponse register(RegisterRequest request) {
         var user = User.builder()
                 .firstname(request.getFirstname())
@@ -36,6 +39,17 @@ public class AuthService {
     }
 
     public AuthenticationResponse authenticate(AuthenticationRequest request) {
-        return null;
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(request.getEmail(), request.getPassword())
+        );
+
+        var user = userRepository.findByEmail(request.getEmail())
+                .orElseThrow();
+
+        var jwtToken = jwtService.generateToken(user);
+
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
     }
 }
