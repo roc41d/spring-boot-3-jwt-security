863b183aa8df18f498073539f7bc5668bb5b51f3
Rocard Fonji <fonkengrocard@gmail.com>
2024-01-15 14:06:19 +0100

    (implementing-jwt-auth-filter): generate jwt token

--- src/main/java/com/sentinelql/security/config/JwtService.java
+++ src/main/java/com/sentinelql/security/config/JwtService.java
@@ -2,11 +2,15 @@ package com.sentinelql.security.config;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import java.security.Key;
+import java.util.Date;
+import java.util.Map;
 import java.util.function.Function;
 
 @Service
@@ -20,6 +24,25 @@ public class JwtService {
     public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
         final Claims claims = extractAllClaims(token);
         return claimsResolver.apply(claims);
+    }
+
+    public String generateToken(UserDetails userDetails) {
+        return generateToken(Map.of(), userDetails);
+    }
+
+    public String generateToken(
+            Map<String, Object> extraClaims,
+            UserDetails userDetails
+    ) {
+        return Jwts
+                .builder()
+                .setClaims(extraClaims)
+                .setSubject(userDetails.getUsername())
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 24 )) // 1 day
+                .signWith(getSignInKey(), SignatureAlgorithm.HS256)
+                .compact(); // generate and return the token
     }
 
     private Claims extractAllClaims(String token) {
