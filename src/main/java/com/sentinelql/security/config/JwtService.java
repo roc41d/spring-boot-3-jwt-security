863c8f3134fd0736d15e1021013a5565e607de4c
Rocard Fonji <fonkengrocard@gmail.com>
2024-01-15 13:28:12 +0100

    (implementing-jwt-auth-filter): extract claims from token

--- src/main/java/com/sentinelql/security/config/JwtService.java
+++ src/main/java/com/sentinelql/security/config/JwtService.java
@@ -1,10 +1,21 @@
 package com.sentinelql.security.config;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
 import org.springframework.stereotype.Service;
 
 @Service
 public class JwtService {
     public String extractUsername(String token) {
         return null;
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts
+                .parser()
+                .setSigningKey(getSignInKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
     }
 }
