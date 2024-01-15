79acfd22f30fede5b0314e8bae51d58dec628f7c
Rocard Fonji <fonkengrocard@gmail.com>
2024-01-15 12:28:22 +0100

    (create-an-app-user-object): create user class and transform it to an entity

---
+++ src/main/java/com/sentinelql/security/user/User.java
@@ -0,0 +1,27 @@
+package com.sentinelql.security.user;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name="_user")
+public class User {
+
+    @Id
+    @GeneratedValue
+    private  Integer id;
+    private String firstname;
+    private String lastname;
+    private String email;
+    private String password;
+}
