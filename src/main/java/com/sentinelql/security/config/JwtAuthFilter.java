c954bc1a95e08f5fc404906f9798da2611838ca1
Rocard Fonji <fonkengrocard@gmail.com>
2024-01-15 12:56:35 +0100

    (implementing-jwt-auth-filter): create the JWT authentication filter

---
+++ src/main/java/com/sentinelql/security/config/JwtAuthFilter.java
@@ -0,0 +1,25 @@
+package com.sentinelql.security.config;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.lang.NonNull;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor // create a constructor using defined final fields
+public class JwtAuthFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(
+            @NonNull HttpServletRequest request,
+            @NonNull HttpServletResponse response,
+            @NonNull FilterChain filterChain
+    ) throws ServletException, IOException {
+
+    }
+}
