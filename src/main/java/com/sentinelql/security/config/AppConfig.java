e3f0d1d16cf95e4990888b61d42e719bc4fd525c
Rocard Fonji <fonkengrocard@gmail.com>
2024-01-15 16:59:04 +0100

    (implementing-jwt-auth-filter): implememnt user details services

---
+++ src/main/java/com/sentinelql/security/config/AppConfig.java
@@ -0,0 +1,21 @@
+package com.sentinelql.security.config;
+
+import com.sentinelql.security.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+@Configuration
+@RequiredArgsConstructor
+public class AppConfig {
+
+    private final UserRepository userRepository;
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return username -> userRepository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
+}
