054484cbee75a43ec3db221b2f60c113093f15a6
Rocard Fonji <fonkengrocard@gmail.com>
2024-01-15 14:12:16 +0100

    (implementing-jwt-auth-filter): check if token is valid

--- src/main/java/com/sentinelql/security/config/JwtService.java
+++ src/main/java/com/sentinelql/security/config/JwtService.java
@@ -45,6 +45,19 @@ public class JwtService {
                 .compact(); // generate and return the token
     }
 
+    public boolean isTokenValid(String token, UserDetails userDetails) {
+        final String username = extractUsername(token);
+        return username.equals(userDetails.getUsername()) && !isTokenExpired(token);
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    private Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
+
     private Claims extractAllClaims(String token) {
         return Jwts
                 .parser()
