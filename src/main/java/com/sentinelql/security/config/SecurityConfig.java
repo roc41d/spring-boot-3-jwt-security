1f824e6565aecc1c93d0ee4b943af917d4c148bb
Rocard Fonji <fonkengrocard@gmail.com>
2024-01-15 21:29:26 +0100

    (implementing-jwt-auth-filter): add security configs

---
+++ src/main/java/com/sentinelql/security/config/SecurityConfig.java
@@ -0,0 +1,45 @@
+package com.sentinelql.security.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private static final String[] WHITELIST_URL = {};
+    private final JwtAuthenticationFilter jwtAuthFilter;
+    private final AuthenticationProvider authenticationProvider;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(req ->
+                    req.requestMatchers(WHITELIST_URL) // white urls in this list
+                        .permitAll()
+                        .anyRequest() //any other request
+                        .authenticated() // should be authenticated
+                )
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(STATELESS) // session should be stateless
+                )
+                .authenticationProvider(authenticationProvider)
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+
+
+
+
+        return http.build();
+    }
+}
